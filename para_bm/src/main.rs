ffcd3f5a1dedb8579cbc71d15ce7af39605a41e1
Victory <git@dfhu.org>
2015-02-17 20:09:17 -0500

    run a sweep of input sizes

--- para_bm/src/main.rs
+++ para_bm/src/main.rs
@@ -1,8 +1,11 @@
+#![feature(std_misc)]
+#![feature(core)]
+
 extern crate para_bm;
 
 use std::time::Duration;
 use std::thread::Thread;
-
+use std::num::Float;
 
 use para_bm::{sleep_ns};
 
@@ -11,17 +14,17 @@ fn fast () {
 }
 
 fn slow () {
-    sleep_ns(1000000000);
+    sleep_ns(10000000);
 }
 
-fn my_bench<F> (f: F) where F: Fn() {
+fn my_bench<F> (f: F) -> f64 where F: Fn() {
 
     // first "throw away" run
     let mut d = Duration::span(|| {
         f();
     });
     let ns = d.num_nanoseconds().unwrap();
-    println!("num nano seconds first run {}", ns);
+    //println!("num nano seconds first run {}", ns);
 
     let mut sum = 0;
     // iterate to get more cache hits, etc...
@@ -34,25 +37,34 @@ fn my_bench<F> (f: F) where F: Fn() {
     }
 
     let avg = (sum as f64) / 10.0;
-    println!("average after first run {}", avg);
+    //println!("average after first run {}", avg);
+
+    return avg;
 }
 
 fn main () {
 
-    println!("Running Threaded slow");
-    my_bench(||{
-        let threads_holder = (0 .. 3).map(|ii| {
-            Thread::scoped(move || {
-                slow();
-            })
-        }).collect::<Vec<_>>();
-    });
-    println!("Done threaded slow");
+    let mut num_ns = 10.0;
 
-    println!("Running serial slow");
-    my_bench(||{
-        let serial_holder = (0 .. 3).map(|ii| {
-            slow();
-        }).collect::<Vec<_>>();
-    });
+    for power in 0 .. 20 {
+        println!("Size.....: {}", num_ns as i64);
+        
+        let t_avg = my_bench(||{
+            let threads_holder = (0 .. 3).map(|_| {
+                Thread::scoped(move || {
+                    sleep_ns(num_ns as i64);
+                })
+            }).collect::<Vec<_>>();
+        });
+        println!("Runtime thread....: {}", t_avg);
+        
+        let s_avg = my_bench(||{
+            let serial_holder = (0 .. 3).map(|_| {
+                sleep_ns(num_ns as i64);
+            }).collect::<Vec<_>>();
+        });
+        println!("Runtime serial....: {}", s_avg);
+        
+        num_ns *= 2.0;
+    }
 }
