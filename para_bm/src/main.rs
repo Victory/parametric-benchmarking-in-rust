c9fce6c5d3f4615e19530c7053b21d4f2de9de19
Victory <git@dfhu.org>
2015-02-18 19:44:04 -0500

    iterate back and forth over the space

--- para_bm/src/main.rs
+++ para_bm/src/main.rs
@@ -28,7 +28,8 @@ fn my_bench<F> (f: F) -> f64 where F: Fn() {
 
     let mut sum = 0;
     // iterate to get more cache hits, etc...
-    for _ in 0 .. 10 {
+    let iters = 100.0;
+    for _ in 0 .. iters as usize {
         d = Duration::span(|| {
             f();
         });
@@ -36,7 +37,7 @@ fn my_bench<F> (f: F) -> f64 where F: Fn() {
         sum = sum + ns;
     }
 
-    let avg = (sum as f64) / 10.0;
+    let avg = (sum as f64) / iters;
     //println!("average after first run {}", avg);
 
     return avg;
@@ -44,11 +45,17 @@ fn my_bench<F> (f: F) -> f64 where F: Fn() {
 
 fn main () {
 
-    let mut num_ns = 10.0;
 
-    for power in 0 .. 20 {
+    let mut max_ns = 10000000.0;
+    let mut min_ns = 0.0;
+    //let mut half_ns;
+    let mut qurt_ns;
+    let mut num_ns = (max_ns + min_ns) / 2.0;
+
+    for power in 0 .. 30 {
         println!("Size.....: {}", num_ns as i64);
         
+
         let t_avg = my_bench(||{
             let threads_holder = (0 .. 3).map(|_| {
                 Thread::scoped(move || {
@@ -56,15 +63,39 @@ fn main () {
                 })
             }).collect::<Vec<_>>();
         });
-        println!("Runtime thread....: {}", t_avg);
+        //println!("Runtime thread....: {}", t_avg);
         
         let s_avg = my_bench(||{
             let serial_holder = (0 .. 3).map(|_| {
                 sleep_ns(num_ns as i64);
             }).collect::<Vec<_>>();
         });
-        println!("Runtime serial....: {}", s_avg);
+        //println!("Runtime serial....: {}", s_avg);
         
-        num_ns *= 2.0;
+        println!("s - t .....: {}", s_avg - t_avg);
+        
+
+
+        if t_avg > s_avg {
+            qurt_ns = (max_ns + num_ns) / 2.0;
+            min_ns = num_ns;
+            num_ns = qurt_ns;
+        } else {
+            qurt_ns = (min_ns + num_ns) / 2.0;
+            max_ns = num_ns;
+            num_ns = qurt_ns;
+        }
+
+        println!("qurt.....: {}", qurt_ns as i64);
+        println!("jump.....: {}", max_ns - min_ns);
+
+        println!("------------------------------------");
+
+        if max_ns - min_ns < 1.0 {
+
+            break;
+        }
+
+
     }
 }
